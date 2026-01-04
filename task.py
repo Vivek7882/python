8002accbcc40bf6a1170aa82c98a40d0e5dc7ae4
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-04 10:00:18 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,19 @@
+collage=input("enter collage name:")
+branch=input("enter branch name:")
+smester=input("enter your semester:")
+
+print(collage)
+print(branch)
+print(smester)
+
+
+
+
+
+
+
+
+
+
+
+
