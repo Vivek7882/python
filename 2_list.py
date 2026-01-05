96d5f1320cfec4c35888afecf316fe5e0edc9ee2
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-05 11:50:22 +0530

    Add files via upload

---
+++ 2_list.py
@@ -0,0 +1,18 @@
+place=['mount abu','agra','jaipur','kshamir','goa','manali','udaipur','goa','agra','goa']
+
+print(place)
+
+print(place.count('goa'))
+
+print(place.count('agra'))
+
+print(place.index('manali'))
+
+print(place.index('jaipur'))
+
+place2 = place.copy()
+
+print(place2,place)
+
+place2.clear()
+print(place2)
