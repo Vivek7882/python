642e369bbfa3be6694b335a2fbc99ceaf6ca24d1
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-05 11:50:49 +0530

    Add files via upload

---
+++ 3_list.py
@@ -0,0 +1,21 @@
+marks=[30,40,50,60,70,80]
+
+print(marks)
+
+marks.insert(0,'70')
+
+marks.append(90)
+
+print(marks)
+
+marks[0]=50
+print(marks)
+
+marks.remove(80)
+print(marks)
+
+marks.pop(0)
+print(marks)
+
+del marks[2]
+print(marks)
