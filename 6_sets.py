1b9b27e132067ab9bc3c049dc21d963c3e0741ec
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-07 11:55:03 +0530

    Add files via upload

---
+++ 6_sets.py
@@ -0,0 +1,8 @@
+fruits = {"apple", "banana", "mango", "orange", "grapes", "pineapple", "papaya", "kiwi", "strawberry", "watermelon"}
+
+fruits.add('apple')#ignor
+fruits.add('banana')#ignor
+
+fruits.add('coconute')# add the item
+fruits.remove('kiwi')
+print(fruits)
