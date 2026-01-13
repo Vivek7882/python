e09b0faebdbc895116d690cd603525448a096e2b
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-13 10:06:41 +0530

    Add files via upload

---
+++ task/p29.py
@@ -0,0 +1,8 @@
+#Write a program to calculate the perimeter of a rectangle.
+
+l=int(input("enter lenght:"))
+b=int(input("enter breadth:"))
+
+ans=2*(l+b)
+
+print(ans)
---
+++ task/p30.py
@@ -0,0 +1,7 @@
+#Write a program to calculate the area of a circle using radius.
+
+r=int(input("enter radius:"))
+
+ans=3.14*r*r
+
+print(ans)
