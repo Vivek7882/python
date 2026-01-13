18155b4d783098dd68e69e1dfe1e9fd380543817
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-13 10:05:52 +0530

    Add files via upload

---
+++ task/p24.py
@@ -0,0 +1,6 @@
+#Write a program to convert kilograms into grams.
+num=int(input("enter gram :"))
+
+kg=num*1000
+
+print(kg,"gram")
---
+++ task/p25.py
@@ -0,0 +1,9 @@
+# Write a program to calculate simple interest using principal, rate, and time.
+
+p=int(input("enter principal:"))
+r=int(input("enter rate:"))
+t=int(input("enter time in year:"))
+
+ans=p*r*t/100
+
+print(ans)
