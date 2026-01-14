98cc3798b76961a75d2a0cfacd1ff73c77162774
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-14 10:08:42 +0530

    Add files via upload

---
+++ 12.1_in_operater.py
@@ -0,0 +1,13 @@
+#write a program to findout whether number exist in list or not using in operator
+
+insert=int(input("enter number:"))
+list1=[1,2,3,5,44,5,6,8,6,5,9,7]
+
+found= insert in list1
+
+print(found)
+
+found= insert not in list1
+
+print(found)
+
