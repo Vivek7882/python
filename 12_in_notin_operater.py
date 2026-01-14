3e3faa68df978fb75b6bc60249ff1c320d4709b8
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-14 10:08:59 +0530

    Add files via upload

---
+++ 12_in_notin_operater.py
@@ -0,0 +1,12 @@
+#write a program to findout name of place existing in tuple or not using in operator
+
+insert=input("enter name:")
+word=("vivek","hardik","tushar","meet","mayur")
+
+found= insert in word
+
+print(found)
+
+found1=insert not in word
+
+print(found1)
