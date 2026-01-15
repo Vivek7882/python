38b172f612e37bd7c253a79137937250f4647b05
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-15 10:10:38 +0530

    Delete p25.py

--- p25.py
+++
@@ -1,9 +0,0 @@
-# Write a program to calculate simple interest using principal, rate, and time.
-
-p=int(input("enter principal:"))
-r=int(input("enter rate:"))
-t=int(input("enter time in year:"))
-
-ans=p*r*t/100
-
-print(ans)
