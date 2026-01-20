bc87fd746e2af6f1a7119261d92e94433fa7f0ba
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-20 12:12:35 +0530

    Add files via upload

---
+++ if_else/18_bigbrother.py
@@ -0,0 +1,10 @@
+# write a program to find out elder brother from given two brother's age.
+
+brother1=int(input("enter 1th brother age:"))
+brother2=int(input("enter 1th brother age:"))
+
+if brother1>brother2:
+    print("1th brother is elder")
+
+if brother1<brother2:
+    print("2th brother is elder")
