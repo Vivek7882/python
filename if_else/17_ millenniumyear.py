a27e8b49a2eea319ab39dffe3a34b9db8872c559
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-20 12:12:07 +0530

    Add files via upload

---
+++ if_else/17_ millenniumyear.py
@@ -0,0 +1,9 @@
+#write a program to find out whether given year is millennium year or not. using if else decision making statements.
+
+num=int(input("enter year:"))
+
+if num%1000==0:
+    print("year is millennium")
+
+else:
+    print("year is not millennium")
