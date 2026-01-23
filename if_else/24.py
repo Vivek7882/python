43bddd98b424b122c9fd6a5e2732b0e1a7d9f4c3
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-23 10:22:59 +0530

    Add files via upload

---
+++ if_else/24.py
@@ -0,0 +1,62 @@
+'''
+write a program to accept birth day and birth month from user as separate input. decide zodiac sign from below table 
+    Aries: March 21-April 19
+    Taurus: April 20-May 20
+    Gemini: May 21-June 21
+    Cancer: June 22-July 22
+    Leo: July 23-August 22
+    Virgo: August 23-September 22
+    Libra: September 23-October 22
+    Scorpio: October 24-November 21
+    Sagittarius: November 22-December 21
+    Capricorn: December 22-January 19
+    Aquarius: January 20-February 18
+    Pisces: February 19-March 20
+
+'''
+day=int(input("enter borth day 1 to 31 :"))
+month=int(input("enter birth month 1 to 12:"))
+
+if (month==3 and day>=21) or (month==4 and day<=19):
+    print("ARIES")
+
+elif(month==4 and day>=20) or (month==5 and day<=20):
+    print("TAURUS")
+
+elif(month==5 and day>=21) or (month==6 and day<=21):
+    print("Gemini")
+
+elif(month==6 and day>=22) or (month==7 and day<=22):
+    print("Cancer")
+
+elif(month==7 and day>=23) or (month==8 and day<=22):
+    print("leo")
+
+elif(month==8 and day>=23) or (month==9 and day<=22):
+    print("vigro")
+
+elif(month==9 and day>=23) or (month==10 and day<=22):
+    print("libra")
+
+elif(month==10 and day>=24) or (month==11 and day<=21):
+    print("scorpio")
+
+elif(month==11 and day>=22) or (month==12 and day<=21):
+    print("Sagittarius")
+
+elif(month==12 and day>=22) or (month==1 and day<=19):
+    print("Capricorn")
+
+elif(month==1 and day>=20) or (month==2 and day<=18):
+    print("Aquarius")
+
+elif(month==2 and day>=19) or (month==3 and day<=20):
+    print("Pisces")
+
+else:
+    print("invlaid data")
+
+
+
+
+
