988616fc1650b1c286b4abb16d1f937e0f776efd
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-01-27 11:30:30 +0530

    Add files via upload

---
+++ Loop/for_loop_3.py
@@ -0,0 +1,29 @@
+#for loop with dictionary
+#write a program to find out list of player who have made more century then given user given value
+odi_centuries = {
+    "sachin tendulkar": 49,
+    "virat kohli": 50,
+    "rohit sharma": 31,
+    "ricky ponting": 30,
+    "sanath jayasuriya": 28,
+    "hashim amla": 27,
+    "ab de villiers": 25,
+    "kumar sangakkara": 25,
+    "chris gayle": 25,
+    "sourav ganguly": 22,
+    "tillakaratne dilshan": 22,
+    "david warner": 22,
+    "quinton de kock": 21,
+    "saeed anwar": 20,
+    "babar azam": 19,
+    "brian lara": 19,
+    "shikhar dhawan": 17,
+    "virender sehwag": 15,
+    "kane williamson": 13,
+    "joe root": 12
+}
+num=int(input("enter centuries :"))
+
+for player in odi_centuries:
+    if odi_centuries[player] > num:
+         print(player,odi_centuries[player])
