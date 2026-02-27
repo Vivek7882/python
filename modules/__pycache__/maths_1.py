83281fd2b19ec3dc32b174c5813bded9f60c808b
Vivek7882 <160031486+Vivek7882@users.noreply.github.com>
2026-02-27 10:25:28 +0530

    Add files via upload

---
+++ modules/__pycache__/maths_1.py
@@ -0,0 +1,10 @@
+import math
+
+number=float(input("enter number:"))
+
+print("ceil number is :",math.ceil(number))
+print("floor value is :",math.floor(number))
+print("truncate value is:",math.trunc(number))
+print("factorial value is:",math.factorial(5)) #always passing value not passing variable
+
+print(f"reminder of 10 divided by 3:",math.fmod(10,3))
